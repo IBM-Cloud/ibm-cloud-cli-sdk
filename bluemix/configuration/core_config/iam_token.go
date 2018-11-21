00c519ac868faebfffadf427a8238eedb2ff6f6b
wwweiwei <wwweiwei@cn.ibm.com>
2018-11-21 14:08:35 +0800

    add more data in IAMTokenInfo

--- bluemix/configuration/core_config/iam_token.go
+++ bluemix/configuration/core_config/iam_token.go
@@ -4,19 +4,47 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"strings"
+	"time"
 )
 
 type IAMTokenInfo struct {
 	IAMID       string       `json:"iam_id"`
+	ID          string       `json:"id"`
+	RealmID     string       `json:"realmid"`
+	Identifier  string       `json:"identifier"`
+	Firstname   string       `json:"given_name"`
+	Lastname    string       `json:"family_name"`
+	Fullname    string       `json:"name"`
 	UserEmail   string       `json:"email"`
 	Accounts    AccountsInfo `json:"account"`
 	Subject     string       `json:"sub"`
 	SubjectType string       `json:"sub_type"`
+	Expiray     jsonTime     `json:"exp"`
+	Issuer      string       `json:"iss"`
+	GrantType   string       `json:"grant_type"`
+	Scope       string       `json:"scope"`
 }
 
 type AccountsInfo struct {
 	AccountID    string `json:"bss"`
 	IMSAccountID string `json:"ims"`
+}
+
+type jsonTime struct {
+	time.Time
+}
+
+func (t jsonTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.Unix())
+}
+
+func (t *jsonTime) UnmarshalJSON(bytes []byte) error {
+	var sec int64
+	if err := json.Unmarshal(bytes, &sec); err != nil {
+		return err
+	}
+	t.Time = time.Unix(sec, 0)
+	return nil
 }
 
 func NewIAMTokenInfo(token string) IAMTokenInfo {
