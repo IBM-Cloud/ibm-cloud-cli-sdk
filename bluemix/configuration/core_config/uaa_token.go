ec17555d0ef4859521cb08ecbfa981a0884b3548
wwweiwei <wwweiwei@cn.ibm.com>
2019-01-02 14:00:06 +0800

    refactor auth repository to support MFA token params

--- bluemix/configuration/core_config/uaa_token.go
+++ bluemix/configuration/core_config/uaa_token.go
@@ -1,11 +1,17 @@
 package core_config
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/common/types"
+)
 
 type UAATokenInfo struct {
 	Username string `json:"user_name"`
 	Email    string `json:"email"`
 	UserGUID string `json:"user_id"`
+	Expiry   time.Time
 }
 
 func NewUAATokenInfo(token string) UAATokenInfo {
@@ -14,11 +20,16 @@ func NewUAATokenInfo(token string) UAATokenInfo {
 		return UAATokenInfo{}
 	}
 
-	var info UAATokenInfo
-	err = json.Unmarshal(tokenJSON, &info)
+	var t struct {
+		UAATokenInfo
+		Expiry types.UnixTime `json:"exp"`
+	}
+	err = json.Unmarshal(tokenJSON, &t)
 	if err != nil {
 		return UAATokenInfo{}
 	}
 
-	return info
+	ret := t.UAATokenInfo
+	ret.Expiry = t.Expiry.Time()
+	return ret
 }
