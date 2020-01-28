01cc1896b2505b33d52fa12ccbe8eb9a37b6b8eb
Philip Schmidt <schmidtp0740@gmail.com>
2020-01-28 13:37:06 -0600

    add comment for ignore gosec

--- plugin_examples/list_plugin/plugin.go
+++ plugin_examples/list_plugin/plugin.go
@@ -77,6 +77,8 @@ func NewHTTPClient(context plugin.PluginContext) *http.Client {
 	transport := bhttp.NewTraceLoggingTransport(
 		&http.Transport{
 			Proxy: http.ProxyFromEnvironment,
+			// ignore the following line from any 'gosec' scanning
+			// as to allow testing in environments that are not setup with TLS properly
 			// #nosec
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: context.IsSSLDisabled(),
