8a4ca9ce152162efa134ac576bf57225f36325cb
Aerex <noamfo@aerex.me>
2022-06-14 17:51:19 -0500

    test(list_plugin): Added translation test for using default language

--- plugin_examples/list_plugin/i18n/i18n_test.go
+++ plugin_examples/list_plugin/i18n/i18n_test.go
@@ -1,6 +1,8 @@
 package i18n_test
 
 import (
+	"os"
+
 	bxi18n "github.com/IBM-Cloud/ibm-cloud-cli-sdk/i18n"
 	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/plugin/pluginfakes"
 	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/plugin_examples/list_plugin/i18n"
@@ -19,6 +21,16 @@ var _ = Describe("i18n", func() {
 		pluginContext = new(pluginfakes.FakePluginContext)
 	})
 
+	Context("Default Config (English)", func() {
+		BeforeEach(func() {
+			pluginContext.LocaleReturns("")
+		})
+		It("should translate message ID \"Created\" successfully in English", func() {
+			t = i18n.Init(pluginContext)
+			Expect(t("Created")).To(Equal("Created"))
+		})
+	})
+
 	Context("User Config", func() {
 
 		Context("When config is set to zh_Hans", func() {
@@ -30,6 +42,80 @@ var _ = Describe("i18n", func() {
 			It("should translate message ID \"Created\" successfully", func() {
 				t = i18n.Init(pluginContext)
 				Expect(t("Created")).To(Equal("创建"))
+			})
+		})
+	})
+
+	Context("Environment variables", func() {
+		var origEnv string
+		Context("LC_ALL is set", func() {
+			BeforeEach(func() {
+				pluginContext.LocaleReturns("")
+				origEnv = os.Getenv("LC_ALL")
+			})
+
+			AfterEach(func() {
+				os.Setenv("LC_ALL", origEnv)
+			})
+
+			Context("When config is set to zh_Hans", func() {
+				BeforeEach(func() {
+					locale = "zh_Hans"
+					pluginContext.LocaleReturns("")
+					os.Setenv("LC_ALL", locale)
+				})
+
+				It("should translate message ID \"Created\" successfully", func() {
+					t = i18n.Init(pluginContext)
+					Expect(t("Created")).To(Equal("创建"))
+				})
+			})
+		})
+
+		Context("LANG is set", func() {
+			BeforeEach(func() {
+				pluginContext.LocaleReturns("")
+				origEnv = os.Getenv("LANG")
+			})
+
+			AfterEach(func() {
+				os.Setenv("LANG", origEnv)
+			})
+
+			Context("When config is set to zh_Hans", func() {
+				BeforeEach(func() {
+					locale = "zh_Hans"
+					pluginContext.LocaleReturns("")
+					os.Setenv("LANG", locale)
+				})
+
+				It("should translate message ID \"Created\" successfully", func() {
+					t = i18n.Init(pluginContext)
+					Expect(t("Created")).To(Equal("创建"))
+				})
+			})
+		})
+	})
+
+	Context("Missing translation ID in Chinese language", func() {
+		var (
+			translationID string
+			translatedMsg string
+		)
+		BeforeEach(func() {
+			locale = "zh_Hans"
+			pluginContext.LocaleReturns(locale)
+		})
+
+		Context("Translation exist in default language (English)", func() {
+			BeforeEach(func() {
+				translationID = "Test translation only in English"
+				translatedMsg = translationID
+			})
+
+			It("should translate message in english", func() {
+				t = i18n.Init(pluginContext)
+				Expect(t(translationID)).To(Equal(translatedMsg))
 			})
 		})
 	})
