8e17c6b2ead89ee29da385aa4fe0abc390a8b305
Aerex <noamfo@aerex.me>
2022-09-01 15:28:22 -0500

    BREAKING CHANGE: Updated to latest version of go-i18n to fix security vulnerability (#323)
    * chore: Updated detect-secrets and hook to latest version
    
    * chore: Updated go-i18n to the latest (2.2.0)
    
    * refactor: Renamed translation files to fit new file format
    
    BREAKING CHANGE: `go-i18n@^v2.0.0` has updated the file format for
    parsing the langage from the filename of the translation file
    
    * chore: Updated bin to generate the translation file using new format
    
    * refactor: Updated translation function to work with new translation
    go-i18n API
    
    * test: Added test for trans method panicking when translation ID cannot be
    found in any file
    
    * chore: Updated .secrets.baseline
    
    * chore: Bumped version to 1.0.0
    
    * fix: Trim suffix and prefix to get lang from filename
    
    * test(i18n): Added unit tests
    
    * fix: Panic only when there is no message to be translated
    
    * test(list_plugin): Added translation test for using default language
    
    * fix: Handled chinese variants cases
    
    * docs: Clean up godocs
    
    * test: Added tests for translation data using templates
    
    * refactor(i18n): Renamed Tfunc to MustTfunc to match original method sig
    
    * docs: Added documentation for using new i18n.T method
    
    * docs: Cleaned up godocs comments
    
    * fix: Ensure correct error return in loadAsset method
    
    * refactor: Replaced panic with a error message for translation

--- i18n/i18n.go
+++ i18n/i18n.go
@@ -1,32 +1,106 @@
 package i18n
 
 import (
+	"encoding/json"
 	"fmt"
 	"path"
-	"sort"
+	"path/filepath"
 	"strings"
 
-	"github.com/nicksnyder/go-i18n/i18n"
-	"github.com/nicksnyder/go-i18n/i18n/language"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
 
 	"github.com/IBM-Cloud/ibm-cloud-cli-sdk/resources"
 )
 
 const (
 	defaultLocale   = "en_US"
-	resourcesSuffix = ".all.json"
+	resourcesSuffix = ".json"
+	resourcesPrefix = "all."
 )
 
-var T i18n.TranslateFunc
+var (
+	bundle                *i18n.Bundle
+	T                     TranslateFunc
+	RESOURCE_PATH         = filepath.Join("i18n", "resources")
+	TRANSLATION_NOT_FOUND = "!!i18N_MESSAGE_NOT_FOUND!!"
+)
 
 func init() {
-	loadAsset("i18n/resources/" + defaultLocale + resourcesSuffix)
-	T = Tfunc(defaultLocale)
+	bundle = Bundle()
+	resource := resourcesPrefix + defaultLocale + resourcesSuffix
+	loadAsset(filepath.Join(RESOURCE_PATH, resource))
+	T = MustTfunc(defaultLocale)
 }
 
-func Tfunc(sources ...string) i18n.TranslateFunc {
-	defaultTfunc := i18n.MustTfunc(defaultLocale)
-	supportedLocales := supportedLocales()
+// Bundle returns an instance of i18n.bundle
+func Bundle() *i18n.Bundle {
+	if bundle == nil {
+		bundle = i18n.NewBundle(language.AmericanEnglish)
+		bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+	}
+	return bundle
+}
+
+// Translate returns a method based on translate method signature in v1.3.0.
+// To allow compatability between v1.3.0 and v2.0+, the `messageId` and `args` parameters are
+// processed to fit with the new Localize API method.
+// @see https://github.com/nicksnyder/go-i18n/blob/v1.3.0/i18n/bundle/bundle.go#L227-L257 for more
+// information on the translate method
+func Translate(loc *i18n.Localizer) TranslateFunc {
+	return func(messageId string, args ...interface{}) string {
+		var pluralCount interface{}
+		var templateData interface{}
+
+		/**
+		 * For the common usecases we can expect two scenarios. Below are two examples:
+		 *  1) T("Cannot login in region {{.REGION}}",  map[string]interface{}{"REGION": "us-south"})
+		 *  2) T("login_fail_count", "2", map[string]interface{}{"Command": "ibmcloud login"})
+		 *
+		 * First paramter is always the `messageId`
+		 * Second paramter can be either pluralCount or templateData.
+		 * Third parameter can be templateData only if the second paramters is the plural count
+
+		 * If we have 2 args than we should expect scenario 2, otherwise we will assume scenario 1
+		 */
+		if argc := len(args); argc > 0 {
+			if isNumber(args[0]) {
+				pluralCount = args[0]
+				if argc > 1 {
+					templateData = args[1]
+				}
+			} else {
+				templateData = args[0]
+			}
+
+		}
+
+		msg, _ := loc.Localize(&i18n.LocalizeConfig{
+			MessageID:    messageId,
+			TemplateData: templateData,
+			PluralCount:  pluralCount,
+		})
+
+		// If no message is returned we can assume that that
+		// the translation could not be found in any of the files
+		// Set the message as !!i18N_MESSAGE_NOT_FOUND!!
+		if msg == "" {
+			msg = TRANSLATION_NOT_FOUND
+		}
+		return msg
+
+	}
+}
+
+// TranslateFunc returns the translation of the string identified by translationID.
+// @see https://github.com/nicksnyder/go-i18n/blob/v1.3.0/i18n/bundle/bundle.go#L19
+type TranslateFunc func(translateID string, args ...interface{}) string
+
+func MustTfunc(sources ...string) TranslateFunc {
+	defaultLocalizer := i18n.NewLocalizer(bundle, defaultLocale)
+	defaultTfunc := Translate(defaultLocalizer)
+
+	supportedLocales, supportedLocalToAsssetMap := supportedLocales()
 
 	for _, source := range sources {
 		if source == "" {
@@ -37,54 +111,61 @@ func Tfunc(sources ...string) i18n.TranslateFunc {
 			return defaultTfunc
 		}
 
-		for _, lang := range language.Parse(source) {
-			switch lang.Tag {
-			case "zh-cn", "zh-sg":
-				lang.Tag = "zh-hans"
-			case "zh-hk", "zh-tw":
-				lang.Tag = "zh-hant"
-			}
+		lang, _ := language.Parse(source)
+		matcher := language.NewMatcher(supportedLocales)
+		tag, _ := language.MatchStrings(matcher, lang.String())
+		assetName, found := supportedLocalToAsssetMap[tag.String()]
 
-			tags := lang.MatchingTags()
-			sort.Strings(tags)
-
-			for i := len(tags) - 1; i >= 0; i-- {
-				tag := tags[i]
-
-				for locale, assetName := range supportedLocales {
-					if strings.HasPrefix(locale, tag) {
-						loadAsset(assetName)
-						return i18n.MustTfunc(locale)
-					}
-				}
-			}
+		if found {
+			loadAsset(assetName)
+			localizer := i18n.NewLocalizer(bundle, source)
+			return Translate(localizer)
 		}
+
 	}
 
 	return defaultTfunc
 }
 
 func loadAsset(assetName string) {
-	bytes, err := resources.Asset(assetName)
-	if err != nil {
-		panic(fmt.Sprintf("Could not load asset '%s': %s", assetName, err.Error()))
+	bytes, assetErr := resources.Asset(assetName)
+	if assetErr != nil {
+		panic(fmt.Sprintf("Could not load asset '%s': %s", assetName, assetErr.Error()))
 	}
 
-	err = i18n.ParseTranslationFileBytes(assetName, bytes)
-	if err != nil {
-		panic(fmt.Sprintf("Could not load translations '%s': %s", assetName, err.Error()))
+	if _, parseErr := bundle.ParseMessageFileBytes(bytes, assetName); parseErr != nil {
+		panic(fmt.Sprintf("Could not load translations '%s': %s", assetName, parseErr.Error()))
 	}
 }
 
-func supportedLocales() map[string]string {
+func supportedLocales() ([]language.Tag, map[string]string) {
+	// When matching against supported language the first language is set as the fallback
+	// so we will initialize the list with English as the first language
+	// @see https://pkg.go.dev/golang.org/x/text/language#hdr-Matching_preferred_against_supported_languages for more information
+	l := []language.Tag{language.English}
 	m := make(map[string]string)
 	for _, assetName := range resources.AssetNames() {
-		locale := normalizeLocale(strings.TrimSuffix(path.Base(assetName), ".all.json"))
-		m[locale] = assetName
+		// Remove the "all." prefix and ".json" suffix to get language/locale
+		locale := normalizeLocale(strings.TrimSuffix(path.Base(assetName), ".json"))
+		locale = strings.TrimPrefix(locale, "all.")
+
+		if !strings.Contains(locale, normalizeLocale(defaultLocale)) {
+			lang, _ := language.Parse(locale)
+			l = append(l, lang)
+			m[lang.String()] = assetName
+		}
 	}
-	return m
+	return l, m
 }
 
 func normalizeLocale(locale string) string {
 	return strings.ToLower(strings.Replace(locale, "_", "-", -1))
+}
+
+func isNumber(n interface{}) bool {
+	switch n.(type) {
+	case int, int8, int16, int32, int64, string:
+		return true
+	}
+	return false
 }
