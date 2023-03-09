78471c1925b9129015449d1ff63f446aeae71f5e
Anupam Pokharel <apokhare@alumni.cmu.edu>
2023-03-09 09:15:19 -0600

    5 - 100

--- bluemix/configuration/persistor.go
+++ bluemix/configuration/persistor.go
@@ -50,10 +50,10 @@ func (dp DiskPersistor) Exists() bool {
 }
 
 func (dp *DiskPersistor) lockedRead(data DataInterface) error {
-	lockCtx, cancelLockCtx := context.WithTimeout(dp.parentContext, 100*time.Second) /* allotting a 30-second timeout means there can be a maximum of 28 failed retrials (each up to 500 ms, as
+	lockCtx, cancelLockCtx := context.WithTimeout(dp.parentContext, 5*time.Second) /* allotting a 30-second timeout means there can be a maximum of 28 failed retrials (each up to 500 ms, as
 	specified after the deferred call to cancelLockCtx). 30 appears to be a conventional value for a parent context passed to TryLockContext, as per docs */
 	defer cancelLockCtx()
-	_, lockErr := dp.fileLock.TryLockContext(lockCtx, 678*time.Millisecond) /* provide a file lock just while dp.read is called, because it calls an unmarshaling function
+	_, lockErr := dp.fileLock.TryLockContext(lockCtx, 100*time.Millisecond) /* provide a file lock just while dp.read is called, because it calls an unmarshaling function
 	The boolean (first return value) can be wild-carded because lockErr must be non-nil when the lock-acquiring fails (whereby the boolean will be false) */
 	if lockErr != nil {
 		fmt.Errorf("Readlock error: " + lockErr.Error())
@@ -78,10 +78,10 @@ func (dp DiskPersistor) Load(data DataInterface) error {
 }
 
 func (dp DiskPersistor) lockedWrite(data DataInterface) error {
-	lockCtx, cancelLockCtx := context.WithTimeout(dp.parentContext, 100*time.Second) /* allotting a 30-second timeout means there can be a maximum of 28 failed retrials (each up to 500 ms, as
+	lockCtx, cancelLockCtx := context.WithTimeout(dp.parentContext, 5*time.Second) /* allotting a 30-second timeout means there can be a maximum of 28 failed retrials (each up to 500 ms, as
 	specified after the deferred call to cancelLockCtx). 30 appears to be a conventional value for a parent context passed to TryLockContext, as per docs */
 	defer cancelLockCtx()
-	_, lockErr := dp.fileLock.TryLockContext(lockCtx, 678*time.Millisecond) /* provide a file lock just while dp.read is called, because it calls an unmarshaling function
+	_, lockErr := dp.fileLock.TryLockContext(lockCtx, 100*time.Millisecond) /* provide a file lock just while dp.read is called, because it calls an unmarshaling function
 	The boolean (first return value) can be wild-carded because lockErr must be non-nil when the lock-acquiring fails (whereby the boolean will be false) */
 	if lockErr != nil {
 		fmt.Errorf("Writelock error: " + lockErr.Error())
