f0c2a67d38e0df5311788697cd2d0973d80015bf
Anupam Pokharel <apokharel@ibm.com>
2023-05-22 16:00:09 -0500

    new context

--- common/rest/client.go
+++ common/rest/client.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 
@@ -43,6 +44,91 @@ func NewClient() *Client {
 	}
 }
 
+func (c *Client) DoWithContext(ctx context.Context, r *Request, respV interface{}, errV interface{}) (*http.Response, error) {
+	retryLimit := 3
+	var resp *http.Response
+	var err error
+	for trial := 0; trial < retryLimit; trial += 1 {
+		resp, err = c.DoWithContextRetry(ctx, r, respV, errV)
+		if err != nil {
+			if strings.Contains(err.Error(), "nexpected EOF") {
+				continue
+			} else { // don't retry if the error is something other than eof
+				return resp, err
+			}
+		} else { // break (and immediately return) if no error
+			break
+		}
+	}
+	return resp, err
+}
+
+func (c *Client) DoWithContextRetry(ctx context.Context, r *Request, respV interface{}, errV interface{}) (*http.Response, error) {
+	req, err := c.makeRequest(ctx, r)
+	if err != nil {
+		return nil, err
+	}
+
+	client := c.HTTPClient
+	if client == nil {
+		client = http.DefaultClient
+	}
+
+	req.Close = true
+	resp, err := client.Do(req)
+	if err != nil {
+		return resp, err
+	}
+	defer func() error {
+		if err := resp.Body.Close(); err != nil {
+			return err
+		}
+		return nil
+	}()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		recvBuf := make([]byte, resp.ContentLength)
+		raw, err := io.ReadFull(resp.Body, recvBuf)
+		if err != nil {
+			return resp, fmt.Errorf("Error reading response: %v", err)
+		}
+
+		if len(recvBuf) > 0 && errV != nil {
+			if json.Unmarshal(recvBuf, errV) == nil {
+				return resp, nil
+			}
+		}
+
+		return resp, &ErrorResponse{resp.StatusCode, string(raw)}
+	}
+
+	if respV != nil {
+		switch respV.(type) {
+		case io.Writer:
+			_, err = io.Copy(respV.(io.Writer), resp.Body)
+		default:
+			// Determine the response type and the decoder that should be used.
+			// If buffer is identified as json, use JSON decoder, otherwise
+			// assume the buffer contains yaml bytes
+			body, isJSON := IsJSONStream(resp.Body, bufferSize)
+			if isJSON {
+				err = json.NewDecoder(body).Decode(respV)
+			} else {
+				err = yaml.NewDecoder(body).Decode(respV)
+			}
+			// For 204 No Content we should not throw an error
+			// if there is an empty response body
+			if err == io.EOF && resp.StatusCode == http.StatusNoContent {
+				err = nil
+			} else if err == io.EOF {
+				err = ErrEmptyResponseBody
+			}
+		}
+	}
+
+	return resp, err
+}
+
 // DoWithContext sends a request and returns a HTTP response whose body is consumed and
 // closed. The context controls the lifetime of the outgoing request and its response.
 //
@@ -52,7 +138,7 @@ func NewClient() *Client {
 // If errV is not nil, the value it points to is JSON decoded when server
 // returns an unsuccessfully response. If the response text is not a JSON
 // string, a more generic ErrorResponse error is returned.
-func (c *Client) DoWithContext(ctx context.Context, r *Request, respV interface{}, errV interface{}) (*http.Response, error) {
+func (c *Client) DoWithContextFormer(ctx context.Context, r *Request, respV interface{}, errV interface{}) (*http.Response, error) {
 	req, err := c.makeRequest(ctx, r)
 	if err != nil {
 		return nil, err
